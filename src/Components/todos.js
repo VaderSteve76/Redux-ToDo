b026cb4431a3622fc349ce32dd0491325c595f80
VaderSteve76 <jarheadm249@VaderSteve76MBP.cfl.rr.com>
2018-10-30 17:33:07 -0400

    todos.js

---
+++ src/Components/todos.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { completeTodo } from '../Actions';
+
+const style = { textDecoration: 'line-through' }
+const Todos = props => {
+  return(
+    <div>
+      {props.todos.map(todo => (
+        <li
+          style={todo.completed ? style : null}
+          onCLick={() => props.completeTodo(todo.id)}
+          key={todo.id}>
+          {todo.text}
+        </li>
+      ))}
+    </div>
+  )
+}
+
+export default connect(null, { completeTodo })(Todos);
