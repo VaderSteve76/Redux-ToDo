143adad7184a22676c144709483a63236c4ed2d1
VaderSteve76 <jarheadm249@VaderSteve76MBP.cfl.rr.com>
2018-10-30 16:57:28 -0400

    App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React, { Component } from 'react';
+import Todos from './Components/todos';
+import TodoForm from './Components/todoForm';
+import { connect } from 'react-redux';
+import { addTodo } from './Actions';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoText: ''
+    }
+  }
+
+  changeHandler = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  addTodo = () => {
+    const { todoText } = this.state;
+    const newTodo = {
+      id: this.props.todos.length + 1,
+      completed: false,
+      text: todoText
+    }
+    this.props.addTodo(newTodo);
+    this.setState({ todoText: '' });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <TodoForm
+          changeHandler={this.changeHandler}
+          todoText={this.state.todoText}
+          addTodo={this.addTodo}
+        />
+        <Todos todos={this.props.todos} />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-
+import { todosReducer } from './Reducers';
 import './index.css';
 import App from './App';
 
 
-// const store = createStore(todosReducer);
+const store = createStore(todosReducer);
 
 ReactDOM.render(
 <Provider store={store}>
