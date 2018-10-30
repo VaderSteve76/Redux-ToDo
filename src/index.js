cdeaaf0b1740309ed8fb4882ef2ec76f3039300c
VaderSteve76 <jarheadm249@VaderSteve76MBP.cfl.rr.com>
2018-10-30 16:02:56 -0400

    index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './App';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const store = createStore(todosReducer);
+
+ReactDOM.render(
+<Provider store={store}>
+  <App />
+</Provider>, 
+document.getElementById('root')
+);
 
 
